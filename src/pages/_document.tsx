298d59420e25bae84367bff87a1e215b11b25a5e
fujitakazuya <88bt3ncs53n@gmail.com>
2020-12-30 16:35:45 +0900

    Add favicon

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
