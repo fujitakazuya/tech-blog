64ff6764a295e14bf2c24e95e2824fe6ad0d6068
fujitakazuya <88bt3ncs53n@gmail.com>
2020-12-31 12:09:18 +0900

    Add noindex,nofollow at 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,21 @@
 /* eslint jsx-a11y/anchor-is-valid: 0 */
 import { VFC } from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
 
 const Custom404: VFC = (): JSX.Element => (
-  <div style={{textAlign: 'center', marginTop: '10rem'}}>
-    <h1>404 Page Not Found.</h1>
-    <Link href="/">
-      <a>トップへ戻る</a>
-    </Link>
-  </div>
+  <>
+    <Head>
+      <meta name="robots" content="noindex,nofollow" />
+      <meta name="googlebot" content="noindex,nofollow" />
+    </Head>
+    <div style={{ textAlign: 'center', marginTop: '10rem' }}>
+      <h1>404 Page Not Found.</h1>
+      <Link href="/">
+        <a>トップへ戻る</a>
+      </Link>
+    </div>
+  </>
 )
 
 export default Custom404
