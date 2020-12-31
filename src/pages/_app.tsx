21a5b384515a656bf7d94b057d3246148e8fa59b
fujitakazuya <88bt3ncs53n@gmail.com>
2021-01-01 01:09:34 +0900

    Add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,26 @@
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import * as gtag from '../lib/gtag'
 import 'modern-css-reset'
 import '../styles/variables.css'
 import '../styles/app.css'
 
-const App = ({ Component, pageProps }: AppProps): JSX.Element => <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
+}
 
 export default App
