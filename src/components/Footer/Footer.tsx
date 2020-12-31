a5647dbfff86bd1881a2e2459feb9c1012522850
fujitakazuya <88bt3ncs53n@gmail.com>
2020-12-31 18:55:25 +0900

    Fix copyright

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css'
 export const Footer: VFC = () => (
   <footer className={styles.footer}>
     <div className={styles.copyright}>
-      <small>&copy; 2020 fujitakazuya</small>
+      <small>&copy; 2020 blog.fuji.gq</small>
     </div>
   </footer>
 )
